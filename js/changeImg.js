593bf189ac7a98198f57fb95e5bb2e58f950c947
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-05-31 04:14:55 +0900

    23-05-31 04:41
    nav바 전체 수정 작업, operation&history html 완료

---
+++ js/changeImg.js
@@ -0,0 +1,38 @@
+$("#pre-ipo-1").hover(function(){
+    $(this).attr("src","./img/operation/pre-ipo/1-.png");
+},function(){
+    $(this).attr("src","./img/operation/pre-ipo/1.png");
+});
+$("#pre-ipo-2").hover(function(){
+    $(this).attr("src","./img/operation/pre-ipo/2-.png");
+},function(){
+    $(this).attr("src","./img/operation/pre-ipo/2.png");
+});
+
+$("#public-1").hover(function(){
+    $(this).attr("src","./img/operation/public/1-.png");
+},function(){
+    $(this).attr("src","./img/operation/public/1.png");
+});
+$("#public-2").hover(function(){
+    $(this).attr("src","./img/operation/public/2-.png");
+},function(){
+    $(this).attr("src","./img/operation/public/2.png");
+});
+
+$("#MandA-1").hover(function(){
+    $(this).attr("src","./img/operation/MandA/1-.png");
+},function(){
+    $(this).attr("src","./img/operation/MandA/1.png");
+});
+$("#MandA-2").hover(function(){
+    $(this).attr("src","./img/operation/MandA/2-.png");
+},function(){
+    $(this).attr("src","./img/operation/MandA/2.png");
+});
+
+$("#structuralization-1").hover(function(){
+    $(this).attr("src","./img/operation/struc/1-.png");
+},function(){
+    $(this).attr("src","./img/operation/struc/1.png");
+});
